511ecc64e97e451cd8b14c9b76baf98dda5ed301
Purecel Radu <79523935+Radu1409@users.noreply.github.com>
2024-01-21 17:40:52 +0200

    Update Dht.cpp

--- Main/Dht.cpp
+++ Main/Dht.cpp
@@ -1,46 +1,5 @@
-//
-//    FILE: dht.cpp
-//  AUTHOR: Rob Tillaart
-// VERSION: 0.1.18
-// PURPOSE: DHT Temperature & Humidity Sensor library for Arduino
-//     URL: http://arduino.cc/playground/Main/DHTLib
-//
-// HISTORY:
-// 0.1.18 version 1.16/17 broke the DHT11 - FIXED
-// 0.1.17 replaced micros() with adaptive loopcount
-//        removed DHTLIB_INVALID_VALUE
-//        added  DHTLIB_ERROR_CONNECT
-//        added  DHTLIB_ERROR_ACK_L  DHTLIB_ERROR_ACK_H
-// 0.1.16 masking unused bits (less errors); refactored bits[]
-// 0.1.15 reduced # micros calls 2->1 in inner loop.
-// 0.1.14 replace digital read with faster (~3x) code => more robust low MHz machines.
-// 0.1.13 fix negative temperature
-// 0.1.12 support DHT33 and DHT44 initial version
-// 0.1.11 renamed DHTLIB_TIMEOUT
-// 0.1.10 optimized faster WAKEUP + TIMEOUT
-// 0.1.09 optimize size: timeout check + use of mask
-// 0.1.08 added formula for timeout based upon clockspeed
-// 0.1.07 added support for DHT21
-// 0.1.06 minimize footprint (2012-12-27)
-// 0.1.05 fixed negative temperature bug (thanks to Roseman)
-// 0.1.04 improved readability of code using DHTLIB_OK in code
-// 0.1.03 added error values for temp and humidity when read failed
-// 0.1.02 added error codes
-// 0.1.01 added support for Arduino 1.0, fixed typos (31/12/2011)
-// 0.1.00 by Rob Tillaart (01/04/2011)
-//
-// inspired by DHT11 library
-//
-// Released to the public domain
-//
-
 #include "Dht.h"
 #include "Logger.h"
-
-/////////////////////////////////////////////////////
-//
-// PUBLIC
-//
 
 int dht::read11(uint8_t pin)
 {
